9ea7a01384681069ca5308552008b2706f25a91c
cjvirtucio87 <cjv287@gmail.com>
2016-09-15 09:11:09 -0700

    Edited private showTaunt and added a getter for memo.

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 var calcModule = (function() {
+  var memo;
+
+  var getMemo = function () {
+      return memo;
+  };
+
   var _taunt = "You couldn't do that yourself, I'll bet!";
 
-  var _getTaunt = function () {
+  var _showTaunt = function () {
     var shouldTaunt = Math.floor(Math.random() * 2);
-
-    return !!shouldTaunt;
+    if (shouldTaunt) {
+      console.log(_taunt);
+    }
   };
 
   var add = function(x, y) {
-    return(x + y);
+    memo = x + y;
+    _showTaunt();
+    return(memo);
   };
 
   return {
-    add: add
+    add: add,
+    getMemo: getMemo
   };
-  
+
 
 })();
+
+
+console.log(calcModule.add(1,2));
+console.log(calcModule.getMemo());
+memo = calcModule.getMemo();
+memo++;
+// getMemo() only returns a copy. State is encapsulated.
+console.log(calcModule.getMemo());
